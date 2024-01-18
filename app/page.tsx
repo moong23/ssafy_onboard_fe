40af9b157b0156b2b14caa1f1f6cec2e299cb01a
moong23 <moonki0623@naver.com>
2024-01-18 17:05:26 +0900

    WIP: mainpage & topbar WIP

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,18 @@
 
 import { fetcher } from "@/api/fetcher";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { IContent } from "@/interface/content";
+import Card from "@/components/card";
+import Topbar from "@/components/topbar";
+import ContentFilterBtn from "@/components/contentFilterBtn";
 
 export default function Home() {
   const router = useRouter();
+  const [dataList, setDataList] = useState([]);
+  const [filter, setFilter] = useState("All");
+
   useEffect(() => {
     const email = localStorage.getItem("email");
     if (email) {
@@ -23,5 +31,43 @@ export default function Home() {
     console.log("test home");
   }, []);
 
-  return <main className="">123</main>;
+  useEffect(() => {
+    // fetcher.post(`/content/create`, {
+    //   title: "test",
+    //   thumbnail: "test",
+    //   content:
+    //     "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.\nlorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.\n",
+    //   date: new Date(),
+    //   writer: localStorage.getItem("email"),
+    // });
+
+    fetcher
+      .get(`/content`)
+      .then((res) => {
+        setDataList(res.data);
+      })
+      .catch((err) => {
+        alert("Internal Server Error");
+      });
+  }, []);
+
+  return (
+    <>
+      <Topbar />
+
+      <main className="w-full px-[200px]">
+        <div className="flex flex-row ">carousel div</div>
+        <div className="flex flex-row gap-8 my-12">
+          <ContentFilterBtn
+            props={{ filter: "All", content: "All", setFilter: setFilter }}
+          />
+        </div>
+        <div className="flex flex-wrap flex-row gap-10 pb-10">
+          {dataList.map((data: IContent) => {
+            return <Card key={data.date} props={data} />;
+          })}
+        </div>
+      </main>
+    </>
+  );
 }
--- components/topbar/index.tsx
+++ components/topbar/index.tsx
@@ -3,15 +3,17 @@ import Link from "next/link";
 
 const Topbar = () => {
   return (
-    <div className="w-full h-[60px] flex flex-row items-center justify-center">
-      <div className="min-w-[1024px]">
-        {Object.entries(TopbarMainMenu).map(([key, value], index) => (
-          <Link href={value.route} key={index}>
-            {value.name}
-          </Link>
-        ))}
+    <nav className="w-full h-[250px] border-b-2 border-b-gray-200 px-[200px] pt-14 flex flex-col justify-between">
+      <h1 className="font-extrabold text-[4rem]">My Blog</h1>
+      <div className="w-full flex flex-row gap-[100px]">
+        <Link className={`px-3 py-1`} href="/">
+          탐색
+        </Link>
+        <Link className={`px-3 py-1`} href="/mypage">
+          마이페이지
+        </Link>
       </div>
-    </div>
+    </nav>
   );
 };
 export default Topbar;
