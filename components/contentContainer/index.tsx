409fe8487c813f40780079c78a636d513b593378
moonki <moonki0623@naver.com>
2024-01-19 08:55:22 +0900

    feat: blog content CSR

---
+++ components/contentContainer/index.tsx
@@ -0,0 +1,12 @@
+"use clinet";
+import { IContent } from "@/interface/content";
+import { useState } from "react";
+const ContentContainer = ({ props }: { props?: IContent }) => {
+  return (
+    <>
+      <>CC</>
+    </>
+  );
+};
+
+export default ContentContainer;
