da936078d17001b20c1d08279a466592e2fc93f8
moonki <moonki0623@naver.com>
2024-01-19 08:55:42 +0900

    feat: content selection filter implemented

--- components/contentFilterBtn/index.tsx
+++ components/contentFilterBtn/index.tsx
@@ -1,12 +1,22 @@
 interface IContentFilterBtnProps {
-  filter: string;
+  filter: boolean;
   content: string;
-  setFilter: React.Dispatch<React.SetStateAction<string>>;
+  setFilter: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const ContentFilterBtn = ({ props }: { props: IContentFilterBtnProps }) => {
+  const handleFilterClick = () => {
+    if (props.content === "All") props.setFilter(false);
+    else props.setFilter(true);
+  };
+
   return (
-    <span className="bg-red-200 px-4 py-1 rounded-3xl font-semibold text-sm">
+    <span
+      onClick={handleFilterClick}
+      className={`${
+        props.filter ? "bg-blue-400 text-white" : "bg-white"
+      } min-w-16 text-center px-4 py-1 rounded-3xl font-semibold text-sm cursor-pointer`}
+    >
       <>{props.content}</>
     </span>
   );
