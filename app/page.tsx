cbc966074740bdbc54275b8d22bc6b4ec6c5bfc7
moong23 <moonki0623@naver.com>
2024-01-18 12:19:17 +0900

    feat: add protected route to mainpage

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,26 @@
 "use client";
 
-import Image from "next/image";
-import { redirect } from "next/navigation";
-import { useLayoutEffect } from "react";
+import { fetcher } from "@/api/fetcher";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
-  useLayoutEffect(() => {
-    const isAuth = true; // TODO : need to change this to login logic
-    redirect("/auth/login");
+  const router = useRouter();
+  useEffect(() => {
+    const email = localStorage.getItem("email");
+    if (email) {
+      fetcher
+        .get(`/isUser?email=${email}`)
+        .then((res) => {
+          if (res.data !== "User exists") throw Error("User not exists");
+        })
+        .catch((err) => {
+          alert(err);
+          console.log(err);
+          router.push("/auth/login");
+        });
+    }
+    console.log("test home");
   }, []);
 
   return <main className="">123</main>;
