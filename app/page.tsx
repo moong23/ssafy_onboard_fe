57300642f90b978c2d0f28603b36f0f61eefe3b5
moong23 <moonki0623@naver.com>
2024-01-19 14:04:29 +0900

    feat: SSR function API call

--- app/page.tsx
+++ app/page.tsx
@@ -34,6 +34,7 @@ export default function Home() {
     fetcher
       .get(`/content`)
       .then((res) => {
+        console.log(res.data);
         setDataList(res.data);
       })
       .catch((err) => {
@@ -41,11 +42,44 @@ export default function Home() {
       });
   };
 
+  const pushContent = async () => {
+    fetcher
+      .post(`/content/create`, {
+        title: "test",
+        thumbnail: "test",
+        content:
+          "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.\nlorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.\n",
+        date: new Date(),
+        writer: localStorage.getItem("email"),
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  };
+
   useEffect(() => {
     confirmUser();
     getContents();
+    // pushContent();
     console.log("test home");
   }, []);
+
+  const getContentByFilter = async () => {
+    if (filter) {
+      fetcher
+        .get(`/content/filter/${localStorage.getItem("email")}`)
+        .then((res) => setDataList(res.data))
+        .catch((err) => console.log(err));
+    } else {
+      // 전체 글 보기
+      getContents();
+    }
+  };
+
+  useEffect(() => {
+    getContentByFilter();
+  }, [filter]);
 
   // fetcher.post(`/content/create`, {
   //   title: "test",
@@ -62,23 +96,26 @@ export default function Home() {
 
       <main className="w-full h-full px-[200px] pb-10">
         <div className="flex flex-row ">carousel div</div>
-        <div className="flex flex-row gap-8 my-12 font-semibold pl-4">
-          필터:
-          <ContentFilterBtn
-            props={{ filter: !filter, content: "All", setFilter: setFilter }}
-          />
-          <ContentFilterBtn
-            props={{ filter: filter, content: "My", setFilter: setFilter }}
-          />
+        <div className="flex flex-row my-12 font-semibold pl-4 justify-between">
+          <span className="flex flex-row gap-8">
+            필터:
+            <ContentFilterBtn
+              props={{ filter: !filter, content: "All", setFilter: setFilter }}
+            />
+            <ContentFilterBtn
+              props={{ filter: filter, content: "My", setFilter: setFilter }}
+            />
+          </span>
+          <button
+            onClick={() => router.push(`/content/create`)}
+            className="bg-lime-200 rounded-3xl min-w-[100px] py-1"
+          >
+            글쓰기
+          </button>
         </div>
-        <div className="flex flex-wrap flex-row gap-10 pb-10">
+        <div className="w-full h-full grid grid-row-3 grid-flow-col gap-4 pb-10 relative">
           {dataList.map((data: IContent) => {
-            return (
-              <Card
-                key={data.date}
-                props={data}
-              />
-            );
+            return <Card key={data.date} props={data} />;
           })}
         </div>
       </main>
